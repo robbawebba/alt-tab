66c775f1625747f9508946338b960c5d9b28151d
robertweber95 <robertweber95@gmail.com>
2016-11-03 22:51:17 -0400

    removed local storage support App now persists in background - will return to storage solutions another day

--- switcher.js
+++ switcher.js
@@ -1,26 +1,15 @@
 var storage = chrome.storage.local;
-var tabs = {"ids": []};
+var tabs = [];
 
 chrome.tabs.onCreated.addListener(function(tab) {
     console.log("Tab Created: " + tab.id);
-    tabs.ids.push(tab.id.toString());
-    saveIDs();
-    console.log(tabs.ids);
-    getIDs();
+    tabs.push(tab.id);
+    console.log(tabs);
 });
 
-function getIDs() {
-    storage.get('ids', function(result) {
-      console.log("IDs retrieved:");
-      console.log(result.ids);
-      tabs = result;
-    });
-}
-
-function saveIDs() {
-    storage.set(tabs, function() {
-      console.log("IDs Saved:");
-      console.log(tabs.ids);
-    });
-}
+chrome.tabs.onRemoved.addListener(function (id, removeInfo) {
+    var removed = tabs.indexOf(id);
+    console.log("removing tab " + id);
+    tabs.splice(removed, 1);
+});
 
