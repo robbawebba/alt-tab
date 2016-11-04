64e82fdd2df5cb0221aeade26a7deda5f99655c2
robertweber95 <robertweber95@gmail.com>
2016-11-04 00:28:46 -0400

    browser action handling

--- switcher.js
+++ switcher.js
@@ -20,7 +20,16 @@ chrome.tabs.onActiveChanged.addListener(function(id, selectInfo) {
     if (oldLocation > -1)  {
         tabs.splice(oldLocation,1)
         tabs.unshift(id);
-        console.log("Tab switch: " + id);
+        console.log("Tab switch: active=" + id);
         console.log(tabs);
     }
 });
+
+chrome.browserAction.onClicked.addListener(swapTabs);
+
+function swapTabs(tab) {
+    chrome.tabs.update(tabs[1], {selected: true});
+    console.log("manually swapped tabs!");
+    console.log("current= " + tab.id + " switching to " + tabs[1]);
+    console.log(tabs);
+}
