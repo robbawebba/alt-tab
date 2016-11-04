932538bbe91f51ab8827eadc08aae1f18a38f026
robertweber95 <robertweber95@gmail.com>
2016-11-03 20:50:47 -0400

    Store tab IDs in local storage

--- switcher.js
+++ switcher.js
@@ -3,5 +3,24 @@ var saveData = {"tabIDs": []};
 
 chrome.tabs.onCreated.addListener(function(tab) {
     console.log("Tab Created: " + tab.id);
+    saveData.tabIDs.push(tab.id.toString());
+    saveIDs();
+    console.log(saveData.tabIDs);
+    getIDs();
 });
+
+function getIDs() {
+    storage.get('tabIDs', function(result) {
+      console.log("IDs retrieved:");
+      console.log(result.tabIDs);
+      saveData = result;
+    });
+}
+
+function saveIDs() {
+    storage.set(saveData, function() {
+      console.log("IDs Saved:");
+      console.log(saveData.tabIDs);
+    });
+}
 
